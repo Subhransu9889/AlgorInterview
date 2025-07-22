044d65fa7596710089eb0bcd6afcad4e4d5db7a2
Subransu <maharanasubhransu6@gmail.com>
2025-07-22 16:16:14 +0530

    Updating the structure of the auth form

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,31 +1,52 @@
-import React from 'react'
-import {FormControl, FormDescription, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Control, Controller, FieldValues, Path} from "react-hook-form";
+import {
+    FormControl,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
 
 interface FormFieldProps<T extends FieldValues> {
     control: Control<T>;
     name: Path<T>;
     label: string;
-    placeholder: string;
-    type? : 'text' | 'password' | 'file';
+    placeholder?: string;
+    type?: "text" | "email" | "password";
+    description?: string;
 }
 
-const FormField = ({control, name, label, placeholder, type = "text"}: FormFieldProps<T>) => {
+const FormField = <T extends FieldValues>({
+                                              control,
+                                              name,
+                                              label,
+                                              placeholder,
+                                              type = "text",
+                                              description,
+                                          }: FormFieldProps<T>) => {
     return (
-        <Controller name={name} control={control} render={({field}) => (
-            <FormItem>
-                <FormLabel className='label'>{label}</FormLabel>
-                <FormControl>
-                    <Input className='input' placeholder={placeholder} type={type} {...field} />
-                </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
-                <FormMessage />
-            </FormItem>
-        )}
+        <Controller
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="label">{label}</FormLabel>
+                    <FormControl>
+                        <Input
+                            className="input"
+                            type={type}
+                            placeholder={placeholder}
+                            {...field}
+                        />
+                    </FormControl>
+                    {description && (
+                        <p className="text-sm text-muted-foreground">{description}</p>
+                    )}
+                    <FormMessage />
+                </FormItem>
+            )}
         />
-    )
-}
-export default FormField
+    );
+};
+
+export default FormField;
