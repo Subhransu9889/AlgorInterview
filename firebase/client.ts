39282662ea4dba8c2c38c6492c8092e703932f6d
Subransu <maharanasubhransu6@gmail.com>
2025-07-23 16:54:30 +0530

    Implementing auth functionality SignUp and SignIn

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBxbmCVr5A3Z3KrV2auDYyAVKz73A1akPo",
-    authDomain: "algorinterview.firebaseapp.com",
-    projectId: "algorinterview",
-    storageBucket: "algorinterview.firebasestorage.app",
-    messagingSenderId: "1096802899057",
-    appId: "1:1096802899057:web:e87b77fd69a43f24db37d0",
-    measurementId: "G-KWQRLE8RXH"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
